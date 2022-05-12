b9dc6dee1654512a143e3379ae5421d68e6dc37d
Shahida <shahidamulla0403@gmail.com>
2022-05-12 12:30:56 +0530

    query selector

--- dom.js
+++ dom.js
@@ -19,21 +19,33 @@ headerTitle.innerText = "bye";*/
 //header.style.borderBottom = 'solid 3px #000';
 
 /*var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-
+console.log(items);*/
+/*
 for(var i = 0; i< items.length; i++){
     items[i].style.color = "green";
     items[i].style.fontWeight = 'bold';
 }*/
 
-var li = document.getElementsByTagName('li');
+/*var li = document.getElementsByTagName('li');
 console.log(li);
 li[2].style.color = "green";
 
 for(var i = 0; i< li.length; i++){
     
     li[i].style.fontWeight = 'bold';
+}*/
+var item= document.querySelector('.list-group-item:nth-child(2)');
+item.style.background ="green";
+var item1= document.querySelector('.list-group-item:nth-child(3)');
+item1.style.display = 'none';
+/*
+//var titles = document.querySelectorAll('.title');
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for (var i = 0 ; i < odd.length;i++){
+    odd[i].style.background = 'yellow';
 }
+var seconditem = document.querySelectorAll('li')
+seconditem[1].style.color = 'green';*/
 
 
 
