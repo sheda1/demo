c5ab79ec42d63f4581fd22d1461cca90c01d1528
Shahida <shahidamulla0403@gmail.com>
2022-05-13 16:48:58 +0530

    multiple user

--- main.js
+++ main.js
@@ -1,28 +1,42 @@
 
 var form = document.getElementById("my-form");
+
 form.addEventListener('submit',localst);
+
 function localst(e){
     e.preventDefault();
     var username = document.getElementById('name').value;
     var useremail = document.getElementById('email').value;
   
-   let myObj ={
+   let myObj =JSON.parse(localStorage.getItem('userData')) || [];
+   myObj.push({
        name : username,
        email : useremail
-   };
-  
-  let myObj_serialized = JSON.stringify(myObj);
-  
-
-   localStorage.setItem("user", myObj_serialized);
-   console.log(localStorage);
-
-   let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
-   console.log(myObj_deserialized)
-
-
+   });
+  localStorage.setItem('userData',JSON.stringify(myObj));
+  display();
+    document.getElementById('my-form').reset;
+    document.getElementById('name').focus();
 
 
 }
+  
+function display(){
+   
+    if(localStorage.getItem('userData')){
+        var output = document.getElementsById('output');
+            output.innerHTML = "";
+       
+ JSON.parse(localStorage.getItem('userData')).forEach(data => {
+         
+        output.innerHTML +=`
+    <ul>
+        <li>${data.name}&nbsp${data.email}</li>
+    </ul>`;
+});
 
+    }
+}
+
+ 
 
