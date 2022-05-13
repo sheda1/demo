d0b3d4f624932c862fd6193ec63ad7fcf55b4463
Shahida <shahidamulla0403@gmail.com>
2022-05-13 13:34:54 +0530

    local storage

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 
-
-function my(){
+var form = document.getElementById("my-form");
+form.addEventListener('submit',localst);
+function localst(e){
+    e.preventDefault();
     var username = document.getElementById('name').value;
     var useremail = document.getElementById('email').value;
-    console.log(useremail);
+  
+   let myObj ={
+       name : username,
+       email : useremail
+   };
+  
+  let myObj_serialized = JSON.stringify(myObj);
+  
 
+   localStorage.setItem("user", myObj_serialized);
+   console.log(localStorage);
 
+   let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+   console.log(myObj_deserialized)
 
-localStorage.setItem('name',username);
-localStorage.setItem('email',useremail);
 
 
 
