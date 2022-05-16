dd64861b814ca0ab8687e1684e6c65774ca32882
Shahida <shahidamulla0403@gmail.com>
2022-05-16 20:05:09 +0530

    edit delete

--- main.js
+++ main.js
@@ -1,41 +1,62 @@
 
 var form = document.getElementById("my-form");
 
+
 form.addEventListener('submit',localst);
 
 function localst(e){
     e.preventDefault();
+   
     var username = document.getElementById('name').value;
     var useremail = document.getElementById('email').value;
   
    let myObj =JSON.parse(localStorage.getItem('userData')) || [];
-   myObj.push({
-       name : username,
-       email : useremail
-   });
-  localStorage.setItem('userData',JSON.stringify(myObj));
-  display();
-    document.getElementById('my-form').reset;
-    document.getElementById('name').focus();
+   let exist = myObj.length && JSON.parse(localStorage.getItem('userData')).some(data => data.email.toLowerCase() == document.getElementById('email').value.toLowerCase());
+   if (!exist){
+    myObj.push({
+        name : username,
+        email : useremail
+    });
+   localStorage.setItem('userData',JSON.stringify(myObj));
+   display();
+     document.querySelector('my-form').reset();
+     document.getElementById('name').focus();
 
+   }else{
+     localStorage.removeItem(document.getElementById('email'));
+     alert('duplicate email');
+   }
 
 }
-  
+ 
 function display(){
+    
    
     if(localStorage.getItem('userData')){
-        var output = document.getElementsById('output');
+        var output = document.getElementById('output');
             output.innerHTML = "";
        
  JSON.parse(localStorage.getItem('userData')).forEach(data => {
          
         output.innerHTML +=`
     <ul>
-        <li>${data.name}&nbsp${data.email}</li>
-    </ul>`;
+        <li>${data.name}&nbsp${data.email}<input type="button" id='edit' value="Edit" onclick="edit()"></input><input type="button" id='delete' value="Delete" onclick="deleteuser()"></input></li>
+    </ul>
+    
+    `;
+  
 });
 
     }
+}
+
+function edit(){
+    document.getElementById('name').focus();
+}
+function deleteuser(){
+    localStorage.removeItem('userData');
+li.remove();
+   
 }
 
  
